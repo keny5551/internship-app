535472359351009d94ea7d00e7082500ac34c561
Michał Jargiło <m.jargilo01@gmail.com>
2023-03-30 05:33:22 +0200

    fix pages linked

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,20 +3,14 @@ import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-
+import { Link } from "react-router-dom";
 const drawerWidth = 240;
-let navItems = ["list of drives", "add drive"];
 
 function DrawerAppBar(props) {
   const { window } = props;
@@ -25,24 +19,6 @@ function DrawerAppBar(props) {
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        Internship - task
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
@@ -69,11 +45,22 @@ function DrawerAppBar(props) {
             Internship - task
           </Typography>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
-            ))}
+            <Button
+              sx={{ color: "#fff" }}
+              component={Link}
+              to="/editadd"
+              color="primary"
+            >
+              Add Drive
+            </Button>
+            <Button
+              sx={{ color: "#fff" }}
+              component={Link}
+              to="/"
+              color="primary"
+            >
+              List Of Drives
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
@@ -93,9 +80,7 @@ function DrawerAppBar(props) {
               width: drawerWidth,
             },
           }}
-        >
-          {drawer}
-        </Drawer>
+        ></Drawer>
       </Box>
     </Box>
   );
